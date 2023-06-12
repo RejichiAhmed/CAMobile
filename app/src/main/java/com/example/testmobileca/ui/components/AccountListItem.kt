6f536a60add8cc39943fcbd128ddc5bf443079cd
DESKTOP-EDGDGGQ\rejic <a.rejichi.ext@hubone.fr>
2023-06-12 09:53:29 +0200

    fix last commit

---
+++ app/src/main/java/com/example/testmobileca/ui/components/AccountListItem.kt
@@ -0,0 +1,93 @@
+package com.example.testmobileca.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.testmobileca.R
+import com.example.testmobileca.data.model.Account
+import com.example.testmobileca.utils.ClickListener
+
+@Composable
+fun AccountListItem(
+    account: Account,
+    onItemClick: ClickListener<Account>
+) {
+    Card(
+        elevation = 4.dp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = Color.White)
+            .padding(start = 64.dp)
+            .clickable { onItemClick.onItemClicked(account) }
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.padding(start = 16.dp, top = 8.dp, end = 16.dp, bottom = 8.dp)
+        ) {
+            Text(
+                modifier = Modifier.padding(start = 16.dp),
+                text = account.label,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Normal,
+                color = Color.Black,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.Start),
+                modifier = Modifier
+                    .wrapContentWidth()
+                    .wrapContentHeight()
+                    .background(Color.Transparent)
+                    .alpha(1f)
+
+            ) {
+
+                Text(
+                    text = account.balance + "€",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Normal,
+                    color = Color.LightGray,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+
+                Box(
+                    modifier = Modifier
+                        .width(20.dp)
+                        .height(20.dp)
+                        .background(Color.Transparent)
+                        .alpha(1f)
+                ) {
+                    Icon(
+                        imageVector =  Icons.Filled.KeyboardArrowRight,
+                        contentDescription = "",
+                        tint = Color.LightGray,
+                    )
+                }
+            }
+
+        }
+    }
+}
